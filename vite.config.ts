722b8adf01837d1ab35d5edcb6389cac5f7f3e10
cuicuiV5_sp7 <cjy1943222010@qq.com>
2023-02-10 21:43:11 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-    base: "/teitter/",
+    base: "/",
     // 配置代理
     server: {
         cors: true,
